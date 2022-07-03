b5bf18391421e32f690a636c4481cf930f447cfc
Ruud Slagers <ruudslagers@gmail.com>
2022-07-03 21:38:25 +0200

    kleiner scherm

--- partials/pagebuilder-components/info-page.php
+++ partials/pagebuilder-components/info-page.php
@@ -7,13 +7,13 @@ $info_text = get_sub_field('info_text');
 
 ?>
 
-<div class="flex flex-col md:flex-row">
-  <div class="basis-full lg:basis-2/3">
+<div class="flex flex-col sm:flex-row">
+  <div class="basis-full sm:basis-2/3">
     <div class="pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-5xl text-gray-500">
       <p><?php echo $info_text;?></p>
     </div>
   </div>  
-  <div class="lg:basis-1/3">
-    <img class="lg:fixed lg:h-screen object-cover grayscale" src="<?php echo $image_url;?>" alt="<?php echo $image_alt;?>">
+  <div class="basis-full sm:basis-1/3">
+    <img class="sm:fixed sm:h-screen object-cover grayscale" src="<?php echo $image_url;?>" alt="<?php echo $image_alt;?>">
   </div>
 </div>
