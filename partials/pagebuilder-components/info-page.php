991ed50cfb8a84814b8604b0483506c9b68f2518
Ruud Slagers <ruudslagers@gmail.com>
2022-07-03 17:28:07 +0200

    Info pages layout v1

--- partials/pagebuilder-components/info-page.php
+++ partials/pagebuilder-components/info-page.php
@@ -6,5 +6,21 @@ $image_alt = get_sub_field('image_right')['alt'];
 $info_text = get_sub_field('info_text');
 
 ?>
-<p><?php echo $info_text;?></p>
-<img src="<?php echo $image_url;?>" class="" alt="<?php echo $image_alt;?>">
+
+
+  <div class="relative bg-white">
+  <div class="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:mx-auto lg:grid lg:grid-cols-2">
+    <div class="lg:col-start-1">
+      <div class="text-base max-w-prose mx-auto lg:ml-auto lg:mr-0">
+       <div class="mt-5 prose prose-indigo text-gray-500">
+          <p><?php echo $info_text;?></p>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="lg:fixed lg:inset-0">
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/3">
+      <img class="h-full w-full object-cover lg:absolute lg:h-full" src="<?php echo $image_url;?>" alt="<?php echo $image_alt;?>">
+    </div>
+  </div>
+</div>
