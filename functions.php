cde76f82bd2ed7c438cf59867f20373ce3ff2bf4
Ruud Slagers <ruudslagers@gmail.com>
2022-07-03 20:32:11 +0200

    info page + footer weggehaald

--- functions.php
+++ functions.php
@@ -157,3 +157,8 @@ if (function_exists('acf_add_options_page')) {
 	// ));
 
 }
+
+function remove_editor() {
+	remove_post_type_support('page', 'editor');
+  }
+  add_action('admin_init', 'remove_editor');
--- partials/pagebuilder-components/info-page.php
+++ partials/pagebuilder-components/info-page.php
@@ -7,20 +7,13 @@ $info_text = get_sub_field('info_text');
 
 ?>
 
-
-  <div class="relative bg-white">
-  <div class="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:mx-auto lg:grid lg:grid-cols-2">
-    <div class="lg:col-start-1">
-      <div class="text-base max-w-prose mx-auto lg:ml-auto lg:mr-0">
-       <div class="mt-5 prose prose-indigo text-gray-500">
-          <p><?php echo $info_text;?></p>
-      </div>
-      </div>
+<div class="flex flex-col md:flex-row">
+  <div class="basis-full lg:basis-2/3">
+    <div class="pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-5xl text-gray-500">
+      <p><?php echo $info_text;?></p>
     </div>
-  </div>
-  <div class="lg:fixed lg:inset-0">
-    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/3">
-      <img class="h-full w-full object-cover lg:absolute lg:h-full" src="<?php echo $image_url;?>" alt="<?php echo $image_alt;?>">
-    </div>
+  </div>  
+  <div class="lg:basis-1/3">
+    <img class="lg:fixed lg:h-screen object-cover grayscale" src="<?php echo $image_url;?>" alt="<?php echo $image_alt;?>">
   </div>
 </div>
