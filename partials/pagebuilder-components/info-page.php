50adf18a3be46ae4e251ef4608057c2b2944a617
Ruud Slagers <ruudslagers@gmail.com>
2022-08-09 20:37:36 +0200

    info + breadcrumbs goed

--- partials/pagebuilder-components/info-page.php
+++ partials/pagebuilder-components/info-page.php
@@ -6,15 +6,12 @@ $image_alt = get_sub_field('image_right')['alt'];
 $info_text = get_sub_field('info_text');
 
 ?>
-<section>
-  <div class="flex flex-col sm:flex-row">
-    <div class="basis-full sm:basis-2/3">
-      <div class="pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-5xl">
+<section class="bg-white-bg">
+    <div class="flex justify-center">
+      <div class="basis-full sm:basis-2/5 text-base pt-20 pb-16 px-4 sm:pt-24 sm:px-6 lg:px-8 lg:max-w-5xl">
+      <div class="sm:invisible breadcrumb text-grey mb-4"><?php get_breadcrumb(); ?></div>
         <p><?php echo $info_text; ?></p>
       </div>
-    </div>
-    <div class="basis-full sm:basis-1/3">
-      <img class="sm:sticky top-0 sm:h-screen object-cover grayscale" src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
     </div>
   </div>
 </section>
