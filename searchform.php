7e10622fa0691ec8389154047803885a66e5730b
Ruud Slagers <ruudslagers@gmail.com>
2022-09-28 20:07:43 +0200

    search geen zoom

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form focus:outline-none focus:outline-0 focus-visible:outline-none focus-visible:outline-0" action="<?php echo home_url('/'); ?>">
     <label>
         <span class="screen-reader-text"><?php echo _x('Search for:', 'label') ?></span>
-        <input id="search-field" type="search" class="search-field focus-visible:outline-none text-base" placeholder="<?php echo esc_attr_x('SEARCH', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
+        <input id="search-field" type="search" class="search-field focus-visible:outline-none text-md no-zoom" placeholder="<?php echo esc_attr_x('SEARCH', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
     </label>
     <!-- <input type="submit" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button') ?>" /> -->
 </form>
