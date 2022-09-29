82261050eaabd8c33b7cbd57583c7540cb70a70b
Amin Pasalic <pasalicamin@gmail.com>
2022-09-29 23:45:02 +0200

    coming soon page updated

--- page-coming-soon.php
+++ page-coming-soon.php
@@ -4,9 +4,15 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <?php get_header('minimal'); ?>
 
-<section class="grid content-center bg-white-bg coming-soon items-center min-h-full justify-center">
+<section style="background-image: url(<?php the_field('bg_image'); ?>" class="bg-no-repeat bg-cover grid content-center bg-white-bg coming-soon items-center min-h-full justify-center relative">
 	<div class="container">
-		<h1 class="mb-8"><?php the_field('titel'); ?></h1>
+		<div class="text-white text-center">
+			<h1 class="text-[20px] mb-[10px]"><?php the_field('titel'); ?></h1>
+			<p class="text-base"><?php the_field('subtitel'); ?></p>
+		</div>
+	</div>
+	<div class="bg-white border border-black left-1/2 translate-x-[-50%] bottom-[10px] w-[90%] absolute lg:translate-x-0 lg:left-auto lg:right-[76px] lg:bottom-[45px] max-w-[343px] p-10 pop-up">
+		<p class="mb-4">HERMAN will be launched soon, sign up to the newsletter and be the first to know</p>
 		<?php
 		if (get_field('shortcode')) {
 			echo do_shortcode(get_field('shortcode'));
