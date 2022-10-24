99bae1fa3d0579c56d69bc4a430c9bf3a6f82898
Ruud Slagers <ruudslagers@gmail.com>
2022-10-24 20:46:48 +0200

    mini cart image full

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -55,7 +55,7 @@ do_action('woocommerce_before_mini_cart'); ?>
 							<div class="flow-root">
 								<ul role="list" class="-mt-6 mb-2 divide-y divide-gray-200 border-t border-gray-200">
 									<li class="flex pt-6 pb-4">
-										<div class="h-24 w-24 flex-shrink-0 overflow-hidden">
+										<div class="h-32 w-20 flex-shrink-0 overflow-hidden">
 											<a href="<?php echo esc_url($product_permalink); ?>">
 												<?php echo $thumbnail; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
 												?>
