fa5a0753def0151723a2d29ab5f92d63e6673e66
Ruud Slagers <ruudslagers@gmail.com>
2022-10-29 10:38:33 +0200

    tableview toegevoegd voor mobiel

--- partials/pagebuilder-components/info-page.php
+++ partials/pagebuilder-components/info-page.php
@@ -8,7 +8,7 @@ $info_text = get_sub_field('info_text');
 ?>
 <section class="bg-white-bg infoList">
   <div class="flex justify-center">
-    <div class="basis-full sm:basis-2/5 text-base pb-16 px-2.5 lg:py-28 lg:max-w-5xl">
+    <div class="basis-full sm:basis-2/5 text-base pb-16 px-2.5 lg:py-28 lg:max-w-5xl tableView">
       <p><?php echo $info_text; ?></p>
     </div>
   </div>
