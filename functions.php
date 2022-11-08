7442b6c40ec482071f1d9432b387cd4c24e058ee
Amin Pasalic <pasalicamin@gmail.com>
2022-11-08 20:50:32 +0100

    if user is logged in move to real homepage

--- functions.php
+++ functions.php
@@ -470,17 +470,19 @@ function teleport_container()
 
 function redirect_to_holding()
 {
-    $holding_page = get_page_by_path('coming-soon');
-    if (($holding_page != NULL) && ($holding_page->post_status == 'publish')) {
-        /* We have a holding page */
-        $current_page = get_post();
+    if (!is_user_logged_in()) {
+        $holding_page = get_page_by_path('coming-soon');
+        if (($holding_page != NULL) && ($holding_page->post_status == 'publish')) {
+            /* We have a holding page */
+            $current_page = get_post();
 
-        if ($holding_page != $current_page) {
-            /* not trying to display the holding page so ok to redirect to holding page */
-            if (current_user_can('delete_users') == false) {
-                /* we are not logged on to an admin account */
-                wp_redirect(get_permalink($holding_page));
-                exit;
+            if ($holding_page != $current_page) {
+                /* not trying to display the holding page so ok to redirect to holding page */
+                if (current_user_can('delete_users') == false) {
+                    /* we are not logged on to an admin account */
+                    wp_redirect(get_permalink($holding_page));
+                    exit;
+                }
             }
         }
     }
