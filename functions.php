87578225d444b0e03f542dfaf1ac639acfe676f3
Ruud Slagers <ruudslagers@gmail.com>
2022-11-23 20:54:04 +0100

    hide in-stock

--- functions.php
+++ functions.php
@@ -469,26 +469,26 @@ function teleport_container()
 }
 
 
-function redirect_to_holding()
-{
-    if (!is_user_logged_in()) {
-        $holding_page = get_page_by_path('coming-soon');
-        if (($holding_page != NULL) && ($holding_page->post_status == 'publish')) {
-            /* We have a holding page */
-            $current_page = get_post();
+// function redirect_to_holding()
+// {
+//     if (!is_user_logged_in()) {
+//         $holding_page = get_page_by_path('coming-soon');
+//         if (($holding_page != NULL) && ($holding_page->post_status == 'publish')) {
+//             /* We have a holding page */
+//             $current_page = get_post();
 
-            if ($holding_page != $current_page) {
-                /* not trying to display the holding page so ok to redirect to holding page */
-                if (current_user_can('delete_users') == false) {
-                    /* we are not logged on to an admin account */
-                    wp_redirect(get_permalink($holding_page));
-                    exit;
-                }
-            }
-        }
-    }
-}
-add_action('get_header', 'redirect_to_holding');
+//             if ($holding_page != $current_page) {
+//                 /* not trying to display the holding page so ok to redirect to holding page */
+//                 if (current_user_can('delete_users') == false) {
+//                     /* we are not logged on to an admin account */
+//                     wp_redirect(get_permalink($holding_page));
+//                     exit;
+//                 }
+//             }
+//         }
+//     }
+// }
+// add_action('get_header', 'redirect_to_holding');
 
 
 add_action('woocommerce_after_order_itemmeta', 'add_item_color', 10, 3);
@@ -513,3 +513,12 @@ function search_products_only($query)
         $query->set('post_type', 'product');
     }
 }
+
+function my_wc_hide_in_stock_message( $html, $text, $product ) {
+	$availability = $product->get_availability();
+	if ( isset( $availability['class'] ) && 'in-stock' === $availability['class'] ) {
+		return '';
+	}
+	return $html;
+}
+add_filter( 'woocommerce_stock_html', 'my_wc_hide_in_stock_message', 10, 3 );
