470dfa64d37a5e18e60e1a7c8558b77924173fd9
Amin Pasalic <pasalicamin@gmail.com>
2022-12-11 21:50:19 +0100

    Update page-template-builder.php

--- page-template-builder.php
+++ page-template-builder.php
@@ -4,8 +4,10 @@
 
 <?php get_header(); ?>
 
+	<?php get_template_part('partials/pagebuilder-loop');
 
-	<?php get_template_part('partials/pagebuilder-loop'); ?>
+	var_dump(get_page_by_title('Customer Care')->ID);
+	?>
 
 <?php
 get_footer();
