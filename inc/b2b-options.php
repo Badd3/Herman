4c07abb84f4e41c20d589564f46b2aa6320e9368
PasalicAmin <43793421+Badd3@users.noreply.github.com>
2023-10-03 18:18:46 +0200

    update quantity input field

--- inc/b2b-options.php
+++ inc/b2b-options.php
@@ -9,3 +9,25 @@ function redirect_location(){
 
 }
 add_action('template_redirect', 'redirect_location');
+
+
+function quantity_field(){
+  global $product; ?>
+  <div class="flex flex-row border-x border-black border-b border-b-black">
+    <div class="border-r-black border-r w-[95px] shrink-0 lg:basis-[95px] py-1.5 px-3 flex flex-col justify-center">  
+      <span class="uppercase">QTY</span>
+    </div>
+    
+      <div>
+    <?php woocommerce_quantity_input(
+      array(
+        'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+        'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+        'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+      )
+    ); ?>
+    </div>
+  </div>
+  <?php
+}
+add_action('woocommerce_after_variations_table', 'quantity_field');
