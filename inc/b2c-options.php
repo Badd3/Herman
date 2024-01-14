da612e6e5a7d8006c77c88fda4b430f64bf4660d
Ruud Slagers <ruudslagers@gmail.com>
2024-01-14 19:21:52 +0100

    Created enable_maintenance in theme

--- inc/b2c-options.php
+++ inc/b2c-options.php
@@ -4,19 +4,21 @@ function wc_remove_all_quantity_fields($return, $product)
     return true;
 }
 
-//Redirect user to login page if not logged in (WHOLESALE)
-function redirect_location() {
-  
-  // Check if the current page is the 'Login' page and the user is logged in
-  if (is_page('coming-soon') && is_user_logged_in()) {
-    wp_redirect('/coming-soon');
-      exit;
-  }
-
-  // If the user is not logged in and not on the 'Login' page, redirect to the 'Login' page
-  if (!is_user_logged_in() && !is_page('coming-soon')) {
-      wp_redirect('/coming-soon');
-      exit;
-  }
+if (get_field('enable_maintenance', 'options')) {
+    //Redirect user to login page if not logged in (WHOLESALE)
+    function redirect_location() {
+    
+        // Check if the current page is the 'Login' page and the user is logged in
+        if (is_page('coming-soon') && is_user_logged_in()) {
+        wp_redirect('/coming-soon');
+            exit;
+        }
+    
+        // If the user is not logged in and not on the 'Login' page, redirect to the 'Login' page
+        if (!is_user_logged_in() && !is_page('coming-soon')) {
+            wp_redirect('/coming-soon');
+            exit;
+        }
+    }
+    add_action('template_redirect', 'redirect_location');
 }
-add_action('template_redirect', 'redirect_location');
