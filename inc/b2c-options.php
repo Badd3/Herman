a47241bf73b79e26bfdb470b04d5f648c7ea2eb4
Ruud Slagers <ruudslagers@gmail.com>
2024-01-14 17:24:03 +0100

    Coming soon template added

--- inc/b2c-options.php
+++ inc/b2c-options.php
@@ -3,3 +3,20 @@ function wc_remove_all_quantity_fields($return, $product)
 {
     return true;
 }
+
+//Redirect user to login page if not logged in (WHOLESALE)
+function redirect_location() {
+  
+  // Check if the current page is the 'Login' page and the user is logged in
+  if (is_page('coming-soon') && is_user_logged_in()) {
+    wp_redirect('/coming-soon');
+      exit;
+  }
+
+  // If the user is not logged in and not on the 'Login' page, redirect to the 'Login' page
+  if (!is_user_logged_in() && !is_page('coming-soon')) {
+      wp_redirect('/coming-soon');
+      exit;
+  }
+}
+add_action('template_redirect', 'redirect_location');
---
+++ page-login.php
@@ -0,0 +1,48 @@
+<?php
+/* Template Name: Login Page */
+get_header('minimal'); ?>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<section style="background-image: url(<?php the_field('bg_image'); ?>);" class="bg-no-repeat bg-cover grid content-center bg-white-bg coming-soon items-center min-h-full justify-center relative coming-soon-mobile">
+  <div class="container">
+    <div class="text-white text-center">
+      <h1 class="text-[20px] mb-[10px]"><?php the_field('titel'); ?></h1>
+      <p class="text-base"><?php the_field('subtitel'); ?></p>
+    </div>
+  </div>
+
+  <div class="text-base bg-white-bg border border-black centered-login-signup w-[90%] max-w-[343px] p-10 pop-up absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <p class="mb-4">Please log in to access the partners webshop.</p>
+
+    <?php do_action('woocommerce_before_customer_login_form'); ?>
+
+    <!-- Display WooCommerce error messages -->
+    <?php wc_print_notices(); ?>
+
+    <form class="woocommerce_form_field login w-full " method="post">
+      <?php do_action('woocommerce_login_form_start'); ?>
+
+      <input type="text" class="text-base w-full py-1 border-b woocommerce-Input woocommerce-Input--text input-text" required placeholder="EMAIL *" name="username" id="username" autocomplete="username" />
+
+      <input class="text-base w-full py-1" type="password" placeholder="PASSWORD *" name="password" id="password" required autocomplete="current-password" />
+
+      <?php do_action('woocommerce_login_form'); ?>
+
+      <?php wp_nonce_field('woocommerce-login', 'woocommerce-login-nonce'); ?>
+
+      <div class="flex items-center justify-between mt-2 text-grey uppercase">
+        <label for="rememberme" class="text-xs">
+          <input type="checkbox" name="rememberme" id="rememberme" value="forever" />
+          <?php esc_html_e('Remember Me', 'woocommerce'); ?>
+        </label>
+      </div>
+
+      <button type="submit" class="woocommerce-button button woocommerce-form-login__submit flex items-center justify-center border border-black px-4 py-1 text-base font-base text-black shadow-sm" name="login" value="<?php esc_attr_e('Log in', 'woocommerce'); ?>"><?php esc_html_e('LOGIN', 'woocommerce'); ?></button>
+
+      <?php do_action('woocommerce_login_form_end'); ?>
+    </form>
+
+  </div>
+</section>
+
+<?php get_footer('minimal'); ?>
