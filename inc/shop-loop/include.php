f84400240aca6eba5f31f5824425cb5b8766ec87
PasalicAmin <43793421+Badd3@users.noreply.github.com>
2024-01-15 21:28:57 +0100

    UDPATE PAGINATION

--- inc/shop-loop/include.php
+++ inc/shop-loop/include.php
@@ -3,7 +3,7 @@ if (is_shop() || is_product_category() || is_product()) {
     remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
     remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
     remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
-    remove_action('woocommerce_after_shop_loop', 'woocommerce_pagination', 10);
+    // remove_action('woocommerce_after_shop_loop', 'woocommerce_pagination', 10);
 
 
     remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
